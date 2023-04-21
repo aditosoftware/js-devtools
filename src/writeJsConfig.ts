eefc0d8205ceedc85d3e0aa3c7037bcd588da564
Michael Kaspera <m.kaspera@adito.de>
2023-04-21 09:58:46 +0200

    fix: wrong jsconfig location, add comments

--- src/writeJsConfig.ts
+++ src/writeJsConfig.ts
@@ -1,6 +1,8 @@
 import { createJsConfigFile } from "../src/jsConfigUtil.js";
 import { exec } from "node:child_process";
 
-const jsconfigTemplatePath: string = '../jsconfig.template.json'
+// the template jsconfig must be located in the root folder of the project, like the resulting jsconfig.json
+const jsconfigTemplatePath: string = 'jsconfig.template.json'
 
+// execute the npm list command and pass the output to the createJsConfigFile method to create the jsconfig.json file
 exec("npm list -j -l", (pError, pStdOut, pStdErr) => createJsConfigFile(pStdOut, pError, pStdErr, () => jsconfigTemplatePath))
