3c5a143a216e93fbfa41a2369e515b360af24b9f
Michael Kaspera <m.kaspera@adito.de>
2023-04-25 15:53:33 +0200

    fix: import statements do not work if type module is missing in package.json

--- src/writeJsConfig.ts
+++ src/writeJsConfig.ts
@@ -1,8 +1,8 @@
-import { createJsConfigFile } from "../src/jsConfigUtil.js";
-import { exec } from "node:child_process";
+const { createJsConfigFile } = require("../src/jsConfigUtil.js");
+const { exec, ExecException } = require("node:child_process");
 
 // the template jsconfig must be located in the root folder of the project, like the resulting jsconfig.json
 const jsconfigTemplatePath: string = 'jsconfig.template.json'
 
 // execute the npm list command and pass the output to the createJsConfigFile method to create the jsconfig.json file
-exec("npm list -j -l", (pError, pStdOut, pStdErr) => createJsConfigFile(pStdOut, pError, pStdErr, () => jsconfigTemplatePath))
+exec("npm list -j -l", (pError: typeof ExecException | null, pStdOut: string, pStdErr: string) => createJsConfigFile(pStdOut, pError, pStdErr, () => jsconfigTemplatePath))
