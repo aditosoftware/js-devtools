ecb6edb1e3d05954df5523bff8c750da244eb560
Michael Kaspera <m.kaspera@adito.de>
2023-04-26 09:12:58 +0200

    fix: deployed package cannot be executed
    Error was missing initializer in cont declaration, plus the require syntax needs the ts file instead of the js file

--- src/writeJsConfig.ts
+++ src/writeJsConfig.ts
@@ -1,8 +1,8 @@
-const { createJsConfigFile } = require("../src/jsConfigUtil.js");
+const { createJsConfigFile } = require("../src/jsConfigUtil.ts");
 const { exec, ExecException } = require("node:child_process");
 
 // the template jsconfig must be located in the root folder of the project, like the resulting jsconfig.json
-const jsconfigTemplatePath: string = 'jsconfig.template.json'
+const jsconfigTemplatePath: string = 'jsconfig.template.json';
 
 // execute the npm list command and pass the output to the createJsConfigFile method to create the jsconfig.json file
 exec("npm list -j -l", (pError: typeof ExecException | null, pStdOut: string, pStdErr: string) => createJsConfigFile(pStdOut, pError, pStdErr, () => jsconfigTemplatePath))
